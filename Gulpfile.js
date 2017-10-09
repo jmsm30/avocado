0ebdee2c79639956a0c93759d974d26ffb462512
Xavier Serrano <zombispormedio007@gmail.com>
2017-10-09 17:37:13 +0200

    Deploy website at Mon Oct 09 2017 17:37:13 GMT+0200 (Hora de verano romance)

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,20 +1,67 @@
 const gulp = require("gulp")
 const del = require("del")
 const shell = require('gulp-shell')
+const file = require('gulp-file')
+const git = require("gulp-git")
+
+const getMarkdownHeader = (pathname, date, title) => `
+---
+path: "/${pathname}"
+date: "${date}"
+title: "${title}"
+---
+`
 
 gulp.task('clean', function(){
     return del([
         ".cache",
+        "public",
+    ])
+})
+
+gulp.task('clean:public', function(){
+    return del([
         "public/*",
         "!.git/",
-        "!.gitignore",
-        "!public/favicon.ico",
-        "!public/README.md"
+        "!.gitignore"
     ])
+})
+
+gulp.task("assets:public", function(){
+    return gulp.src("assets/*")
+            .pipe(gulp.dest("public"))
 })
 
 gulp.task('build', shell.task([
     'npm run build'
 ]))
 
-  gulp.task('default', ['clean', 'build']);
+gulp.task("create:post", function(){
+    const argv = require('yargs').argv;
+    const title = argv.t || "Untitled Post"
+    const pathname = argv.p || title.replace(/ +/g, '-').toLowerCase()
+    const now = new Date()
+    const mdHeader = getMarkdownHeader(pathname, now.toISOString(), title)
+
+    return file(`${now.getTime()}-${pathname}.md`, mdHeader, {src: true})
+            .pipe(gulp.dest("./content/posts"))
+})
+
+gulp.task("clone:public", function(cb){
+    require('dotenv').config()
+
+    git.clone(process.env.REPO, {args: "./public"}, function(err){
+        if(err) throw err
+        cb()
+    })
+})
+
+gulp.task('add:public', function(){
+    return gulp.src('.')
+      .pipe(git.add());
+});
+
+gulp.task('commit:public', function(){
+    return gulp.src('.',  {cwd: "public"})
+      .pipe(git.commit(`Deploy website at ${new Date()}`));
+});

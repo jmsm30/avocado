5cf09fa9840f83154e80b510da97d103213602eb
Xavier Serrano <zombispormedio007@gmail.com>
2017-10-12 11:23:05 +0200

    test continuous delivery

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,5 @@ export default () =>
     <div>
         <h1>Hi! What's up? 😜💕</h1>
         <p>What do I like to do? Lots of course but definitely enjoy building websites.</p>
-        <div style={{fontSize: "0.8em"}}><i>LOCO</i>!!! CI works</div>
+        <div style={{fontSize: "0.8em"}}> CI works</div>
     </div>

267fc0e360d810273bdb146bf2b2959d62b6617a
Xavier Serrano <zombispormedio007@gmail.com>
2017-10-14 15:15:47 +0200

    change title

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,4 +4,5 @@ export default () =>
     <div>
         <h1>Hi! What's up? 😜💕</h1>
         <p>What do I like to do? Lots of course but definitely enjoy building websites.</p>
+        <p> This website is on building</p>
     </div>
